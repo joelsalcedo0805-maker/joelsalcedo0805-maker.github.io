f831d9689079f15a0366fb8a8ddb891c69558a4f
joelsalcedo0805-maker <joelsalcedo0805@gmail.com>
2025-12-01 12:52:32 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,21 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("jpos-cache-v1").then(cache => {
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('jpos-cache').then(cache => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
+        './',
+        './index.html',
+        './manifest.json',
+        './service-worker.js',
+        './icons/icon-192.png',
+        './icons/icon-512.png'
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
