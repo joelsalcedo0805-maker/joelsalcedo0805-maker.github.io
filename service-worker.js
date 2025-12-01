55f31103e6a274c94725549d44f7e97571035d6d
joelsalcedo0805-maker <joelsalcedo0805@gmail.com>
2025-12-01 13:51:17 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,31 @@
-const CACHE_NAME = "jpos-cache-v1";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./service-worker.js",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
-  );
   self.skipWaiting();
+  event.waitUntil(
+    caches.open("jpos-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ]);
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      caches.match('./index.html').then((resp) => resp || fetch(event.request).catch(() => caches.match('./index.html')))
-    );
-    return;
-  }
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then((res) => {
+      return (
+        res ||
+        fetch(event.request).catch(() =>
+          caches.match("./index.html")
+        )
+      );
+    })
   );
 });
